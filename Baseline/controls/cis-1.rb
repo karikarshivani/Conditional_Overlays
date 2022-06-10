e58131d03a9c6bd0514a0eef081ad47a592133dd
karikarshivani <karikarshivani@gmail.com>
2022-06-09 21:30:55 -0400

    control name
    Signed-off-by: karikarshivani <karikarshivani@gmail.com>

--- Baseline/controls/cis-1.rb
+++ Baseline/controls/cis-1.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 
 control "cis-1" do
-  title "Print the Profile Name"
+  title "Print the current input"
   impact 0.5
   
   puts "======================\nINPUT: " + input("test-input") + "\n======================"
